3ab7e6db26187150bad8ecf2f53caef3c92bfdcf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-05 20:03:00 +0900

    import chats components #4

--- assets/router/router.js
+++ assets/router/router.js
@@ -1,10 +1,13 @@
-import Vue from 'vue/dist/vue.esm.js'
-import VueRouter from 'vue-router'
-import Index from '../components/web/Index.vue'
-import About from '../components/web/About.vue'
-import Contact from '../components/web/Contact.vue'
+import Vue from 'vue/dist/vue.esm.js';
+import VueRouter from 'vue-router';
+import Index from '../components/web/Index.vue';
+import About from '../components/web/About.vue';
+import Contact from '../components/web/Contact.vue';
 
-Vue.use(VueRouter)
+import ChatRoomIndex from '../components/chats/Index.vue';
+import ChatRoom from '../components/chats/ChatRoom.vue';
+
+Vue.use(VueRouter);
 
 export default new VueRouter({
   mode: 'history',
@@ -12,5 +15,7 @@ export default new VueRouter({
     { path: '/', component: Index },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
+    { path: '/chats', component: ChatRoomIndex },
+    { path: '/chats/:id', component: ChatRoom, name: 'chatroom' },
   ],
 })
