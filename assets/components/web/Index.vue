2125812cf151cb6d31345f8ab55217f53cbf6225
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-05 19:52:11 +0900

    using login to GitHub Account #3

--- assets/components/web/Index.vue
+++ assets/components/web/Index.vue
@@ -2,5 +2,44 @@
 <div>
     <h1>Index Pages</h1>
     <p>AskHub is asking programming & code!</p>
+    <button v-if="!this.$store.state.session" v-on:click="loginAskHub" class="btn btn-primary">Login to GitHub</button>    
 </div>    
 </template>
+
+<script>
+import router from '../../router/router';
+export default {
+    moutnted: function() {
+        this.userLogin();
+    },
+    methods: {
+        userLogin: function() {
+            const firebase = this.$store.state.firebase;
+            firebase.auth().onAuthStateChanged((response) => {
+                if (response) {
+                    this.$store.commit('getSession', true);
+                    router.push({path: '/'});
+                } else {
+                    this.$store.commit('getSession', false);
+                    router.push({path: '/'});
+                }
+            }, (error) => {
+                console.log(error);
+            });
+        },
+        loginAskHub: function() {
+            const firebase = this.$store.state.firebase;
+
+            const provider = this.$store.state.provider;
+
+            firebase.auth().signInWithPopup(provider).then((response) => {
+                this.$store.commit('loginAskHub', response.user.uid);
+                this.$store.commit('setUserName', response.additionalUserInfo.profile.name);
+            }, (error) => {
+                console.log(error)
+            })
+            this.userLogin();
+        }
+    }
+}
+</script>
